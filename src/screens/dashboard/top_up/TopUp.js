910f11474b7ea9403175e26cd03d74edb4ee64f5
ahikmah <imadlz15@gmail.com>
2021-06-13 10:32:05 +0700

    feat : add top up UI

--- src/screens/dashboard/top_up/TopUp.js
+++ src/screens/dashboard/top_up/TopUp.js
@@ -1,17 +1,47 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import {
   View,
   Text,
   ScrollView,
   TouchableOpacity,
   StatusBar,
+  TextInput,
+  Keyboard
 } from 'react-native';
 import {Icon} from 'native-base';
 import classes from './Styles';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 
+import SwipeUpDownModal from 'react-native-swipe-modal-up-down';
+
 const TopUp = props => {
   const {navigation} = props;
+  const [addNew, setAddNew] = useState('');
+  const [isFilled, setIsFilled] = useState(false)
+  const [errorMessage, setErrorMessage] = useState();
+  const [showModal, setShowModal] = useState(false);
+  const [animateModal, setAnimateModal] = useState(false);
+
+  const isValidNum = num => {
+    return !!num.match(/^[0-9]*$/);
+  };
+
+  useEffect(()=>{
+    if(Number(addNew)>10000){
+      setIsFilled(true)
+    } else {
+      setIsFilled(false)
+    }
+  },[addNew])
+
+
+  const submitHandler = () => {
+    if(!addNew){
+      setErrorMessage('Please fill in this field')
+    } else if(Number(addNew)<10000){
+      setErrorMessage('the minimum topup amount is Rp10.000')
+    }
+  };
 
   const instructionData = [
     {
@@ -58,13 +88,24 @@ const TopUp = props => {
           barStyle="dark-content"
         />
         <View style={classes.headeruppersection}>
-          <TouchableOpacity
-            onPress={() => {
-              props.navigation.goBack();
-            }}>
-            <Icon name="arrow-back-outline" style={classes.back} />
-          </TouchableOpacity>
-          <Text style={classes.title}>Top Up</Text>
+          <View style={{flexDirection: 'row'}}>
+            <TouchableOpacity
+              onPress={() => {
+                props.navigation.goBack();
+              }}>
+              <Icon name="arrow-back-outline" style={classes.back} />
+            </TouchableOpacity>
+            <Text style={classes.title}>Top Up</Text>
+          </View>
+          <View style={{flexDirection: 'row'}}>
+            {/* <Text style={classes.title}>Instant Top up</Text> */}
+            <TouchableOpacity
+              onPress={() => {
+                setShowModal(true);
+              }}>
+              <Icon name="card-outline" style={classes.back} />
+            </TouchableOpacity>
+          </View>
         </View>
         <View style={classes.headerbottomsection}>
           <TouchableOpacity style={classes.addbtn}>
@@ -89,6 +130,53 @@ const TopUp = props => {
           </View>
         ))}
       </View>
+
+      <SwipeUpDownModal
+        modalVisible={showModal}
+        PressToanimate={animateModal}
+        MainContainerModal={classes.mainModal}
+        duration={300}
+        ContentModal={
+          <View style={classes.containerContent}>
+            <Text style={classes.titleModal}>Instant Top Up</Text>
+            <View style={classes.createNewSection}>
+              <Text style={classes.rupiah}>Rp</Text>
+              <TextInput
+                value={addNew}
+                style={classes.numberInput}
+                placeholder="0"
+                keyboardType="numeric"
+                onChangeText={p => {
+                  isValidNum(p) && setAddNew(p);
+                }}
+                onPressIn={() => setErrorMessage('')}
+              />
+            </View>
+              {errorMessage?<Text style={classes.errorMessage}>{errorMessage}</Text>:null}
+            <TouchableOpacity
+              style={isFilled?classes.sendOn:classes.sendOff}
+              onPress={submitHandler}
+              disabled={isFilled?false:true}
+              >
+              
+              <Text style={isFilled?classes.txtTopUpOn:classes.txtTopUpOff}>Top Up Now</Text>
+            </TouchableOpacity>
+          </View>
+        }
+        HeaderStyle={classes.headerContent}
+        ContentModalStyle={classes.Modal}
+        HeaderContent={
+          <View style={{alignItems: 'center'}}>
+            <View style={classes.line} />
+          </View>
+        }
+        onClose={() => {
+          setAddNew('')
+          setErrorMessage('')
+          setShowModal(false);
+          setAnimateModal(false);
+        }}
+      />
     </ScrollView>
   );
 };
