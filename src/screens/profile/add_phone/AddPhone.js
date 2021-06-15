98aa0ddc3d72f108be82d7b9bbc975803515cc0f
Nugroho-00 <snugroho211@gmail.com>
2021-06-15 09:30:52 +0700

    new

--- src/screens/profile/add_phone/AddPhone.js
+++ src/screens/profile/add_phone/AddPhone.js
@@ -9,64 +9,21 @@ import {
 import classes from './Styles';
 import Header from '../../../components/header/Header';
 import Ionicons from 'react-native-vector-icons/Ionicons';
-<<<<<<< HEAD
-import {Toast} from 'native-base';
-import {connect} from 'react-redux';
-import {API_URL} from '@env';
-import axios from 'axios';
-import {phoneValidation} from '../../../services/validation/Validation';
-
-const AddPhone = props => {
-  const {navigation} = props;
-  const UpdateData = props.userReducers.user?.data[0];
-  const [phone, setPhone] = useState();
-
-  const FormData = require('form-data');
-  const data = new FormData();
-  const config = {
-    method: 'PATCH',
-    url: `${API_URL}/profile/edit`,
-    headers: {
-      authorization: `Bearer ${props.loginReducers.user?.token}`,
-    },
-    data: data,
-  };
-
-  function submitHandler() {
-    data.append('phone', phone);
-
-    axios(config)
-      .then(res => {
-        console.log(res);
-        if (res.data.message === 'Data Updated') {
-          Toast.show({
-            text: 'Success',
-            type: 'success',
-            textStyle: {textAlign: 'center'},
-            duration: 2000,
-          });
-        }
-      })
-      .catch(err => {
-        console.log(err);
-      });
-  }
-
-=======
 import {connect} from 'react-redux';
 import axios from 'axios';
 import {API_URL} from '@env';
 import CustomModal from '../../../components/modal/CustomModal';
 
-
 const AddPhone = props => {
   const {navigation} = props;
   const data = props.userReducers.user?.data;
-  const [phone, setPhone] = useState(data?.phone.replace(/\B(?=(\d{4})+(?!\d))/g, '-'));
-  const [phoneValue, setPhoneValue] = useState(data?.phone)
+  const [phone, setPhone] = useState(
+    data?.phone.replace(/\B(?=(\d{4})+(?!\d))/g, '-'),
+  );
+  const [phoneValue, setPhoneValue] = useState(data?.phone);
   const [errorMessage, setErrorMessage] = useState('');
-  const [isFilled, setIsFilled] = useState(false)
-  const [confirmModal, setConfirmModal] = useState(false)
+  const [isFilled, setIsFilled] = useState(false);
+  const [confirmModal, setConfirmModal] = useState(false);
 
   const addCommas = num => num.toString().replace(/\B(?=(\d{4})+(?!\d))/g, '-');
   const removeNonNumeric = num => num.toString().replace(/[^0-9]/g, '');
@@ -75,39 +32,40 @@ const AddPhone = props => {
     num.substring(0) !== '0' && setPhone(addCommas(removeNonNumeric(num)));
   };
 
-  const handleValue = num =>{
-    setPhoneValue(num.toString().replace('-', ''))
-  }
+  const handleValue = num => {
+    setPhoneValue(num.toString().replace('-', ''));
+  };
   // console.log(phone);
-  const submitHandler=()=>{
+  const submitHandler = () => {
     let config = {
       method: 'PATCH',
       url: `${API_URL}/profile/edit`,
       headers: {
         authorization: `Bearer ${props.loginReducers.user?.token}`,
       },
-      data: {phone:phoneValue.replace("-","")},
+      data: {phone: phoneValue.replace('-', '')},
     };
-    axios(config).then(res=>{
-      setConfirmModal(false)
-      return props.getUserHandler(props.loginReducers.user.token)}
-    ).catch(err => console.log({err}))
-  }
+    axios(config)
+      .then(res => {
+        setConfirmModal(false);
+        return props.getUserHandler(props.loginReducers.user.token);
+      })
+      .catch(err => console.log({err}));
+  };
 
-  useEffect(()=>{
+  useEffect(() => {
     console.log(phoneValue);
-    if(phoneValue.length>12){
-      setErrorMessage('Max phone number length is 12')
-      setIsFilled(false)
-    } else if(phoneValue.length<9){
-      setErrorMessage('Min phone number length is 10')
-      setIsFilled(false)
+    if (phoneValue.length > 12) {
+      setErrorMessage('Max phone number length is 12');
+      setIsFilled(false);
+    } else if (phoneValue.length < 9) {
+      setErrorMessage('Min phone number length is 10');
+      setIsFilled(false);
     } else {
-      setIsFilled(true)
-      setErrorMessage('')
+      setIsFilled(true);
+      setErrorMessage('');
     }
-  },[phoneValue])
->>>>>>> e6de667393c0a06abac567ca225b43574e503716
+  }, [phoneValue]);
   return (
     <ScrollView>
       <Header isBack={true} title="Add Phone Number" navigation={navigation} />
@@ -129,35 +87,33 @@ const AddPhone = props => {
               placeholder="Enter your phone number"
               placeholderTextColor="rgba(169, 169, 169, 0.8)"
               keyboardType="phone-pad"
-<<<<<<< HEAD
-              onChangeText={value => {
-                setPhone(value);
-              }}
-=======
               value={phone}
               onPressIn={() => setErrorMessage('')}
-              onChangeText={value => {handleChange(value); handleValue(value)}}
->>>>>>> e6de667393c0a06abac567ca225b43574e503716
+              onChangeText={value => {
+                handleChange(value);
+                handleValue(value);
+              }}
             />
           </View>
-          {errorMessage?<Text style={classes.errorMessage}>{errorMessage}</Text>:null}
+          {errorMessage ? (
+            <Text style={classes.errorMessage}>{errorMessage}</Text>
+          ) : null}
         </View>
         <View>
           <TouchableOpacity
             style={{
               ...classes.submitbtn,
-              backgroundColor: isFilled ?'rgba(99, 121, 244, 1)':'rgba(218, 218, 218, 1)',
+              backgroundColor: isFilled
+                ? 'rgba(99, 121, 244, 1)'
+                : 'rgba(218, 218, 218, 1)',
             }}
-<<<<<<< HEAD
-            onPress={async () => {
-              await submitHandler();
-            }}>
-            <Text style={{...classes.submitbtntext, color: 'white'}}>
-=======
             disabled={false}
-            onPress={()=>setConfirmModal(true)}>
-            <Text style={{...classes.submitbtntext, color:isFilled?'white':'rgba(136, 136, 143, 1)'}}>
->>>>>>> e6de667393c0a06abac567ca225b43574e503716
+            onPress={() => setConfirmModal(true)}>
+            <Text
+              style={{
+                ...classes.submitbtntext,
+                color: isFilled ? 'white' : 'rgba(136, 136, 143, 1)',
+              }}>
               Submit
             </Text>
           </TouchableOpacity>
@@ -184,10 +140,6 @@ const mapStatetoProps = state => ({
   loginReducers: state.loginReducers,
   userReducers: state.userReducers,
 });
-<<<<<<< HEAD
-
-const connectedAddPhone = connect(mapStatetoProps)(AddPhone);
-=======
 const mapDispatchToProps = dispatch => ({
   getUserHandler: token => {
     dispatch(getUser(token));
@@ -197,6 +149,5 @@ const connectedAddPhone = connect(
   mapStatetoProps,
   mapDispatchToProps,
 )(AddPhone);
->>>>>>> e6de667393c0a06abac567ca225b43574e503716
 
 export default connectedAddPhone;
